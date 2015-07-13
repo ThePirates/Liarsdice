f029ef88736c0cf538b9ae9062bd458ce5f8616d
filipematos95 <fm1995@gmail.com>
2015-07-14 00:37:57 +0100

    Main begin interface done. Players seek bar added.

--- src/com/example/liarsdice/MainActivity.java
+++ src/com/example/liarsdice/MainActivity.java
@@ -1,16 +1,50 @@
 package com.example.liarsdice;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
+
 
 public class MainActivity extends Activity {
-
+	
+	private SeekBar players_bar;
+	private TextView players_number_view; 
+	private Button start_game;
+	int players_number = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);	
+		
+		Initialize_Variables();
+		
+		players_bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+	        
+	           
+	      @Override
+	      public void onProgressChanged(SeekBar seekbar, int progresValue, boolean fromUser) {
+	           players_number = progresValue;
+	           players_number_view.setText("Number of players:" + players_number);
+	        }
+	     
+	      @Override
+          public void onStartTrackingTouch(SeekBar seekBar) {
+	    	   Log.i("SEEKBAR", "START");
+          }
+          
+          @Override
+          public void onStopTrackingTouch(SeekBar seekBar) {
+        	  Log.i("SEEKBAR","STOP");
+          }
+       });
 	}
 
 	@Override
@@ -30,5 +64,12 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void Initialize_Variables(){
+		players_bar = (SeekBar) findViewById(R.id.playersbar);
+		players_number_view = (TextView) findViewById(R.id.players);
+		players_number_view.setText("Number of players: 0");
+		start_game = (Button) findViewById(R.id.startgame);
 	}
 }
