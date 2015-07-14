f9645e75c34d708febc14e73979aba8f2f8ff86a
filipematos95 <fm1995@gmail.com>
2015-07-14 01:35:52 +0100

    Game activity added, it receives from the main menu the number of players.

--- src/com/example/liarsdice/MainActivity.java
+++ src/com/example/liarsdice/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
+import android.content.Intent;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
@@ -13,7 +16,8 @@ import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-	
+	public final static String NUMBER_PLAYERS = "com.example.liarsdice.PLAYERSNUMBER";
+	private final static String TAG = "Activity";
 	private SeekBar players_bar;
 	private TextView players_number_view; 
 	private Button start_game;
@@ -42,11 +46,61 @@ public class MainActivity extends Activity {
           
           @Override
           public void onStopTrackingTouch(SeekBar seekBar) {
+        	  
         	  Log.i("SEEKBAR","STOP");
           }
        });
+		
+		Button start_game = (Button) findViewById(R.id.startgame);
+		start_game.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, Game.class);
+				String number = players_number_view.getText().toString();
+				intent.putExtra(NUMBER_PLAYERS, number);
+				startActivity(intent);
+			}		
+		});
+
 	}
 
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.i(TAG, "Entered the onResume() method");
+
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		Log.i(TAG, "Entered the onPause() method");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+
+		Log.i(TAG, "Entered the onStop() method");
+	}
+
+	@Override
+	public void onRestart() {
+		super.onRestart();
+
+		Log.i(TAG, "Entered the onRestart() method");
+
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		Log.i(TAG, "Entered the onDestroy() method");
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
